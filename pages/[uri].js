8d49af17f5eb581af124a3287705c1f3b769ca25
gujaratimedia108 <119147682+gujaratimedia108@users.noreply.github.com>
2023-06-18 14:11:23 +0530

    Update [uri].js

--- pages/[uri].js
+++ pages/[uri].js
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 
 export default function SlugPage({ post }) {
 useEffect(()=>{
-  location = `https://www.infolate.com/${post?.slug}`
+  location = `https://www.gujaratiayurved.com/${post?.slug}`
 },[])
   return (
     <div>
